d90e6a2e3601ddedc9a159f35071911fb6390523
rusldrim <116371573+rusldrim@users.noreply.github.com>
2023-03-31 13:49:27 +0300

    Create 02-video.js

---
+++ src/js/02-video.js
@@ -0,0 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const TIME_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe); 
+
+const getCurrentTime = (currentTime) => {
+  const seconds = currentTime.seconds;
+  localStorage.setItem(TIME_KEY, JSON.stringify(seconds));
+};
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(TIME_KEY)) || 0);
+
